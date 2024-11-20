3e3b79e57699ea21c9671229970d2aed49264dcd
m01101000 <96919599+m01101000@users.noreply.github.com>
2024-11-20 13:34:27 +0100

    Add files via upload

---
+++ luftfederung/src/main/java/de/bht_berlin/paf/luftfederung/LuftfederungApplication.java
@@ -0,0 +1,13 @@
+package de.bht_berlin.paf.luftfederung;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LuftfederungApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LuftfederungApplication.class, args);
+	}
+
+}
---
+++ luftfederung/src/test/java/de/bht_berlin/paf/luftfederung/LuftfederungApplicationTests.java
@@ -0,0 +1,13 @@
+package de.bht_berlin.paf.luftfederung;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LuftfederungApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
